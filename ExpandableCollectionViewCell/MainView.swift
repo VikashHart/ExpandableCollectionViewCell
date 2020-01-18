47ec26b782357024bc597f17067789351fc2d5d0
Vikash Hart <vikashhart@gmail.com>
2020-01-17 23:59:03 -0500

    main view added

---
+++ ExpandableCollectionViewCell/MainView.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+class MainView: UIView {
+
+    lazy var headerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let font = "AvenirNext-Regular" //dont use if you want the default system font, or change it to any other font you want to use. Look here for a list of available fonts: http://iosfonts.com
+
+    lazy var headerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Header Goes Here"
+        label.font = UIFont(name: font, size: 18)
+        label.textAlignment = .center
+        label.textColor = .white
+        label.backgroundColor = .clear
+        label.numberOfLines = 0
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let cell = "CollectionViewCell"
+
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.black
+        collectionView.register(DefaultCollectionViewCell.self, forCellWithReuseIdentifier: cell)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        commonInit()
+    }
+
+    private func commonInit() {
+        backgroundColor = .white
+        setupViews()
+    }
+
+    private func setupViews() {
+        setupHeader()
+        setupHeaderLabel()
+        setupCollectionView()
+    }
+
+    private func setupHeader() {
+        addSubview(headerView)
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 44)
+            ])
+    }
+
+    private func setupHeaderLabel() {
+        headerView.addSubview(headerLabel)
+        NSLayoutConstraint.activate([
+            headerLabel.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            headerLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor)
+            ])
+    }
+
+    private func setupCollectionView() {
+        addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+    }
+}
