dbc0ae33a4a0f04a8339d44ba8faba4bb2c97b66
Vikash Hart <vikashhart@gmail.com>
2020-01-18 00:41:28 -0500

    readme test

--- ExpandableCollectionViewCell/ViewModelExpandable.swift
+++ ExpandableCollectionViewCell/ViewModelExpandable.swift
@@ -4,4 +4,5 @@ protocol ViewModelExpandable {
     var hiddenCells: [ExpandableCell] { get set }
     var expandedCells: ExpandableCell? { get set }
     var toggleBool: Bool { get set }
+    var animation: CellAnimatable { get }
 }
